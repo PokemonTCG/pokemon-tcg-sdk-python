0f0a60ce8838bc0030b105f017879a9588c715e6
Andrew Backes <backes.andrew@gmail.com>
2018-03-02 14:16:42 -0600

    New package due to release issues

--- pokemontcgsdk/config.py
+++ pokemontcgsdk/config.py
@@ -8,7 +8,7 @@
 # http://www.opensource.org/licenses/MIT-license
 # Copyright (c) 2016, Andrew Backes <backes.andrew@gmail.com>
 
-__version__ = "1.4.0"
+__version__ = "1.4.1"
 __pypi_packagename__ = "pokemontcgsdk"
 __github_username__ = "PokemonTCG"
 __github_reponame__ = "pokemon-tcg-sdk-python"
